89a9278a7d7d4e7cf7a3a20fdcd90e00f4038264
57716_vaibhav <vaibhavkt2@gmail.com>
2022-04-13 10:27:47 +0530

    Added

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  firstname="";
+  lastname="";
+  Address="";
+  phone="";
+  country="";
+  subject="";
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
